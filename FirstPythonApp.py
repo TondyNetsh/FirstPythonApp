5e50ab128d33dac8a99f53815f61bba40cbe0329
Rotondwa Netshimbupfe <rt.netsh@live.com>
2021-10-18 11:17:13 +0200

    Add project files.

---
+++ FirstPythonApp.py
@@ -0,0 +1,25 @@
+#First Python comment
+
+#name = "Rotondwa";
+#surname = "Netshimbupfe"
+
+num1 = 11
+num2 = 21.7
+shopName = "Woolworths"
+intials = 'R'
+tuples = ("Mercedez Benz","BMW","AUDI")
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+
+print()
+print("Hello, " + name + " " + surname)
+
+a = 5
+
+print(type(a))
+print(type(num2))
+print(type(shopName))
+print(type(intials))
+print(type(tuples))
+
+
